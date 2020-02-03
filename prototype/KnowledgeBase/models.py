d11a8f66586102b4404302c8ed8718b8ae9e1959
Scott Hardin <hardin.scott93@gmail.com>
2020-02-03 09:15:20 -0500

    think i finished up models just need the pages

--- prototype/KnowledgeBase/models.py
+++ prototype/KnowledgeBase/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -7,9 +8,15 @@ class Asset(models.Model):
     assetName = models.CharField(max_length=30)
 
 
+class Department(models.Model):
+    departmentName = models.CharField(max_length=50)
+    departmentAssets = models.ManyToManyField(Asset)
+
+
 class Threat(models.Model):
     threatName = models.CharField(max_length=100)
     assetKey = models.ForeignKey(Asset, on_delete=models.CASCADE)
+    departmentKey = models.ForeignKey(Department, on_delete=models.PROTECT)
 
 
 class Advice(models.Model):
@@ -20,8 +27,16 @@ class Advice(models.Model):
 class Question(models.Model):
     questionText = models.CharField(max_length=200)
     assetKey = models.ForeignKey(Asset, on_delete=models.CASCADE)
+    departmentKey = models.ManyToManyField(Department)
 
 
 class Answers(models.Model):
     answerText = models.CharField(max_length=200)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    departmentKey = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+    canAnswer = models.BooleanField
+
