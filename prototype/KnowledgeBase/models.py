1f86d19502301847aa72d55958499c0c009b85e2
Scott Hardin <hardin.scott93@gmail.com>
2020-02-02 20:27:13 -0500

    added models and an index page

--- prototype/KnowledgeBase/models.py
+++ prototype/KnowledgeBase/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Asset(models.Model):
+    assetName = models.CharField(max_length=30)
+
+
+class Threat(models.Model):
+    threatName = models.CharField(max_length=100)
+    assetKey = models.ForeignKey(Asset, on_delete=models.CASCADE)
+
+
+class Advice(models.Model):
+    adviceText = models.CharField(max_length=200)
+    threatKey = models.ForeignKey(Threat, on_delete=models.CASCADE)
+
+
+class Question(models.Model):
+    questionText = models.CharField(max_length=200)
+    assetKey = models.ForeignKey(Asset, on_delete=models.CASCADE)
+
+
+class Answers(models.Model):
+    answerText = models.CharField(max_length=200)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
