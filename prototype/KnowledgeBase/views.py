fcf9e6cc6438dfb524c82c740cd7d374919a8b82
woods-taylor <43648944+Woods-Taylor@users.noreply.github.com>
2020-02-03 23:01:54 -0500

    added template file and reqest for homepage

--- prototype/KnowledgeBase/views.py
+++ prototype/KnowledgeBase/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("index.")
+    return render(request, 'index.html')
 
--- prototype/prototype/urls.py
+++ prototype/prototype/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('KnowledgeBase', include('KnowledgeBase.urls'))
+    path('KnowledgeBase/', include('KnowledgeBase.urls'))
 ]
