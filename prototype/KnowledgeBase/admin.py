270c3ff0e2cf482c70200b797769ecb53fc6a35e
Scott Hardin <hardin.scott93@gmail.com>
2020-02-05 09:28:10 -0500

    made it easier to add objects through admin interface

--- prototype/KnowledgeBase/admin.py
+++ prototype/KnowledgeBase/admin.py
@@ -3,8 +3,29 @@ from django.contrib import admin
 # Register your models here.
 from .models import Asset, Question, Advice, Threat, Answer
 
-admin.site.register(Asset)
-admin.site.register(Question)
-admin.site.register(Answer)
-admin.site.register(Threat)
-admin.site.register(Advice)
+
+class assetAdmin(admin.ModelAdmin):
+    list_display = ("assetName",)
+
+
+class questionAdmin(admin.ModelAdmin):
+    list_display = ("assetKey", "questionText",)
+
+
+class threatAdmin(admin.ModelAdmin):
+    list_display = ("assetKey", "threatName",)
+
+
+class answerAdmin(admin.ModelAdmin):
+    list_display = ("question", "answerText",)
+
+
+class adviceAdmin(admin.ModelAdmin):
+    list_display = ("threatKey", "adviceText",)
+
+
+admin.site.register(Asset, assetAdmin)
+admin.site.register(Question, questionAdmin)
+admin.site.register(Answer, answerAdmin)
+admin.site.register(Threat, threatAdmin)
+admin.site.register(Advice, adviceAdmin)
